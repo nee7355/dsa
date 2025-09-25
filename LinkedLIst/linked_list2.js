3fe3cbdf5ca3a6713da246a5f68ba5be765b8761
Neeraj <neekumar334@gmail.com>
2025-09-25 08:59:19 +0530

    solution of geekforgeek exercise

--- LinkedLIst/linked_list2.js
+++ LinkedLIst/linked_list2.js
@@ -26,11 +26,11 @@ class LinkdeList {
       current.next = node;
     }
 
-    size(){
-        if(this.head===null) return 0;
+    size(head=this.head){
+        if(head===null) return 0;
 
         let size = 0;
-        let current = this.head;
+        let current = head;
         while(current){
             current=current.next;
             size++;
@@ -94,6 +94,125 @@ class LinkdeList {
       return this.head;
     }
 
+    // Program for Nth node from the end of a Linked List
+    // Given a Linked List of M nodes and a number N, find the value at the Nth node from the end of the Linked List. If there is no Nth node from the end, print -1.
+
+    /* there is two method to  findout  nth node from end
+     1. One pass method;
+     2. Two pass method;
+    */
+// two pass method
+    nthNodeFromEnd(n){
+      if(this.head === null) return null;
+      const size = this.size();
+
+      if(n>size) return null;
+      
+      let current = this.head ;
+      let idx = 0;
+      while(idx<size-n){
+        current = current.next;
+        idx++;
+      }
+      return current.data;
+    }
+
+    nthNodeFromEnd2(n){
+      if(this.head===null) return null;
+
+      let first = this.head;
+      let second = this.head;
+
+      for(let i=0;i<n; i++){
+        if (first===null) return null;
+        first = first.next;
+      }
+      while(first !== null){
+           first = first.next;
+           second = second.next;
+      }
+
+      return second.data;
+    }
+
+    // Delete Last Occurrence
+
+    deleteLastOccurence(key){
+      if(!this.head) return;
+
+      let prev = null;
+      let current = this.head;
+
+      let lastPrev = null;
+      let lastNode = null;
+      
+      while(current){
+        if(current.data===key){
+          lastPrev = prev;
+          lastNode = current;
+        }
+        prev = current;
+        current = current.next;
+      }
+
+      if(!lastNode) return;
+      if(!lastPrev){
+        this.head = this.head.next;
+      }else{
+        lastPrev.next = lastNode.next;
+      }
+    }
+
+    printList(){
+      if(!this.head) return;
+
+      let list = '';
+      let current  = this.head;
+      while(current){
+        list += current.data + ' > '
+        current  = current.next;
+      }
+
+      console.log(list.slice(0, -3));
+    }
+
+    // Delete Middle
+
+    deleteMiddle(){
+      if(!this.head) return;
+      let size = this.size();
+      let mid = Math.floor(size/2);
+
+      let idx = 0;
+      let current = this.head;
+      while(current && idx<mid-1){
+        current = current.next;
+        idx++;
+      }
+      current.next = current.next.next;
+    }
+    // Merge a linked list into another linked list at alternate positions
+    mergeAlternatePos(head1, head2){
+        if(!head1) return head2;
+        if(head2 === null) return head1;
+        let head1Current =head1;
+        let head2Current = head2;
+        
+        while(head1Current&& head2Current){
+
+          let head1Next = head1Current.next;
+          let head2Next = head2Current.next;
+
+          head1Current.next = head2Current;
+          if(head1Next===null) break;
+            head2Current.next = head1Next;
+            head1Current = head1Next;
+            head2Current = head2Next; 
+        }
+
+        return head1;
+    }
+
 }
 
 const list = new LinkdeList();
@@ -102,6 +221,17 @@ list.addData(20)
 list.addData(30)
 list.addData(40)
 list.addData(50);
+// list.addData(10)
 
-console.log(list);
-console.log(list.reverseList());
+// const list2 = new  LinkdeList();
+// list2.addData(60);
+// list2.addData(70);
+// list2.addData(80);
+
+// console.log(list.reverseList());
+
+// console.log(list.nthNodeFromEnd2(6))
+// list.deleteLastOccurence(10);
+// list.deleteMiddle();
+// list.mergeAlternatePos(list.head, list2.head);
+list.printList();
